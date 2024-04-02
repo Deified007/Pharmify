8d4a383067f1389108d0f7e264d3990f41d98259
Deified007 <132086767+Deified007@users.noreply.github.com>
2024-04-02 09:51:39 +0530

    Add files via upload

---
+++ Controllers/DetailMenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmify.Controllers
+{
+    public class DetailMenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
